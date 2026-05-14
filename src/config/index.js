0dfc55e55051b5cf8e550d42a95763bef860e2a1
grimaldi2706 <grimaldi2706@gmail.com>
2026-05-14 15:17:32 -0500

    feat: implement site configuration and content pages for English and Spanish localization

--- src/config/index.js
+++ src/config/index.js
@@ -2,11 +2,21 @@ import common from './common.json';
 import es from './es.json';
 import en from './en.json';
 
+const pageModules = import.meta.glob('./pages/**/*.json', { eager: true });
+
+const pages = { es: {}, en: {} };
+for (const [path, mod] of Object.entries(pageModules)) {
+  const parts = path.replace(/\\/g, '/').split('/');
+  const lang = parts[2];
+  const name = parts[3].replace('.json', '');
+  pages[lang][name] = mod.default;
+}
+
 const config = {
   common,
   languages: {
-    es,
-    en
+    es: { ...es, pages: pages.es },
+    en: { ...en, pages: pages.en }
   }
 };
 
